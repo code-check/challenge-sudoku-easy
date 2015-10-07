91ed0fdc518d8a7aaca529530c80cb0b8e4f6dad
NckHmml <NckHmml@users.noreply.github.com>
2015-10-07 10:23:38 +0900

    Minor code change

--- src/main/scala/Solver.scala
+++ src/main/scala/Solver.scala
@@ -10,56 +10,54 @@ object Solver {
         .toArray
 
     // Check the cells
-    for {
-      v <- 1 to 9
-      c <- 0 until 9
-    } {
-      var count = 0
-      val sy = c - (c % 3)
-      val sx = 3 * (c % 3)
-      for {
-        x <- 0 until 3
-        y <- 0 until 3
-      } {
-        if (parsed(x + sx)(y + sy) == v) {
-          count += 1
-          if (count > 1)
-            return false
-        }
-      }
-    }
+    def checkCells: Boolean =
+      (for {
+        v <- 1 to 9
+        c <- 0 until 9
+      } yield {
+          var count = 0
+          val sy = c - (c % 3)
+          val sx = 3 * (c % 3)
+          for {
+            x <- 0 until 3
+            y <- 0 until 3
+          } {
+            if (parsed(x + sx)(y + sy) == v)
+              count += 1
+          }
+
+          count <= 1
+        }).foldLeft(true)((l, r) => l & r)
 
     // Check the columns
-    for {
-      v <- 1 to 9
-      x <- 0 until 9
-    } {
-      var count = 0
-      for (y <- 0 until 9)
-        if (parsed(x)(y) == v)
-        {
-          count += 1
-          if (count > 1)
-            return false
-        }
-    }
+    def checkColumns: Boolean =
+      (for {
+        v <- 1 to 9
+        x <- 0 until 9
+      } yield {
+          var count = 0
+          for (y <- 0 until 9)
+            if (parsed(x)(y) == v)
+              count += 1
+
+          count <= 1
+        }).foldLeft(true)((l, r) => l & r)
 
     // Check the rows
-    for {
-      v <- 1 to 9
-      y <- 0 until 9
-    } {
-      var count = 0
-      for (x <- 0 until 9)
-        if (parsed(x)(y) == v)
-        {
-          count += 1
-          if (count > 1)
-            return false
-        }
-    }
+    def checkRows: Boolean =
+      (for {
+        v <- 1 to 9
+        y <- 0 until 9
+      } yield {
+          var count = 0
+          for (x <- 0 until 9)
+            if (parsed(x)(y) == v)
+              count += 1
 
-    true
+          count <= 1
+        }).foldLeft(true)((l, r) => l & r)
+
+    checkCells & checkColumns & checkRows
     //END_CHALLENGE
   }
 }
