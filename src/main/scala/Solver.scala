2f73ef1d053477c286bae0889b20eab14c3f21de
NckHmml <NckHmml@users.noreply.github.com>
2015-10-07 15:37:56 +0900

    Expanded the test case

--- src/main/scala/Solver.scala
+++ src/main/scala/Solver.scala
@@ -9,55 +9,40 @@ object Solver {
         .grouped(9)
         .toArray
 
+    // Check the sudoku for 0's
+    val checkCount: Boolean =
+      parsed.flatten.map(_.toInt).sum == 9 * ((9 * (9 + 1)) / 2)
+
     // Check the cells
-    def checkCells: Boolean =
-      (for {
+    val checkCells: Boolean =
+      !(for {
         v <- 1 to 9
         c <- 0 until 9
       } yield {
-          var count = 0
-          val sy = c - (c % 3)
-          val sx = 3 * (c % 3)
-          for {
-            x <- 0 until 3
-            y <- 0 until 3
-          } {
-            if (parsed(x + sx)(y + sy) == v)
-              count += 1
-          }
-
-          count <= 1
-        }).foldLeft(true)((l, r) => l & r)
+          val y = c - (c % 3)
+          val x = 3 * (c % 3)
+          (for (ix <- 0 until 3; iy <- 0 until 3) yield (ix, iy))
+            .map { case (ix, iy) => (ix + (x - (x % 3)), iy + (y - (y % 3))) }
+            .count { case (ix, iy) => parsed(ix)(iy) == v }
+        }).exists(_ >= 2)
 
     // Check the columns
-    def checkColumns: Boolean =
-      (for {
+    val checkColumns: Boolean =
+      !(for {
         v <- 1 to 9
         x <- 0 until 9
-      } yield {
-          var count = 0
-          for (y <- 0 until 9)
-            if (parsed(x)(y) == v)
-              count += 1
-
-          count <= 1
-        }).foldLeft(true)((l, r) => l & r)
+      } yield (0 until 9).count(parsed(x)(_) == v)
+        ).exists(_ >= 2)
 
     // Check the rows
-    def checkRows: Boolean =
-      (for {
+    val checkRows: Boolean =
+      !(for {
         v <- 1 to 9
         y <- 0 until 9
-      } yield {
-          var count = 0
-          for (x <- 0 until 9)
-            if (parsed(x)(y) == v)
-              count += 1
+      } yield (0 until 9).count(parsed(_)(y) == v)
+        ).exists(_ >= 2)
 
-          count <= 1
-        }).foldLeft(true)((l, r) => l & r)
-
-    checkCells & checkColumns & checkRows
+    checkCount & checkCells & checkColumns & checkRows
     //END_CHALLENGE
   }
 }
